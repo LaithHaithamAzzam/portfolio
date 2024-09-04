35a2ebef4631cb8ffd3e0c66770b19eb509efdf6
Laith Azzam <Laetheazzam.la.la@gmail.com>
2024-09-04 18:18:34 +0300

    splashscreencommet

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"f6344b75dcf861d8bf1f1322780b8811f982e3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1469109910"
+    serviceWorkerVersion: "2138867250"
   }
 });
